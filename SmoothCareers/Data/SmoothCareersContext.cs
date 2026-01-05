4bee3fd13791dc173fdeb49bef1800b47d6224da
sabri <2304046b@student.tp.edu.sg>
2026-01-05 22:28:10 +0800

    Add CRUD Razor pages for Employer management
    Implemented full CRUD functionality for Employer entities, including Create, Edit, Delete, Details, and Index Razor pages. Updated navigation to include Employers section. Added Employer DbSet to SmoothCareersContext for EF Core support. Restricted create/edit/delete actions to Administrators and included validation and overposting protection.

--- SmoothCareers/Data/SmoothCareersContext.cs
+++ SmoothCareers/Data/SmoothCareersContext.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SmoothCareers.Configurations.Entities;
 using SmoothCareers.Data;
+using SmoothCareers.Domain;
 
 namespace SmoothCareers.Data
 {
@@ -29,5 +30,6 @@ namespace SmoothCareers.Data
 
 
         }
+        public DbSet<SmoothCareers.Domain.Employer> Employer { get; set; } = default!;
     }
 }
